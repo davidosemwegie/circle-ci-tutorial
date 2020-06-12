8dae8736620ff592ed981408138dc16781203f71
David Osemwegie <dosemwegie@gmail.com>
2020-06-11 22:37:40 -0600

    Update sum.test.ts

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -4,6 +4,6 @@ test("Adds 2 + 2 to equal 4", () => {
   expect(sum(2, 2)).toBe(4)
 })
 
-test("Adds 3 + 2 to equal 4", () => {
-  expect(sum(3, 2)).toBe(18)
+test("Adds 3 + 2 to equal 5", () => {
+  expect(sum(3, 2)).toBe(5)
 })
